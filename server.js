64687bbd40ac7c85ce8899b294b2bc04f148414b
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2024-04-08 15:14:38 +0200

    m

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 // const cors = require('cors');
 const app = express();
+
 const port = process.env.Port || 5000;
 
 // app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 
 app.get('/api/contacts', (req,res) =>{
     res.status(200).send('Port is running')
