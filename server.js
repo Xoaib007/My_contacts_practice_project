8f833006de92edb13cf484dc6c798df2f3296304
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2024-04-08 15:38:40 +0200

    m

--- server.js
+++ server.js
@@ -6,11 +6,8 @@ const app = express();
 const port = process.env.Port || 5000;
 
 // app.use(cors());
-app.use(express.json());
 
-app.get('/api/contacts', (req,res) =>{
-    res.status(200).send('Port is running')
-});
+app.use('/api/contacts', require("./routes/contactRoutes.js"));
 
 app.listen(port, () =>{
     console.log(`port ${port}`)
